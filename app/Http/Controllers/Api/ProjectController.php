894773eea674432db71ad125aa70881970ff02a3
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-08-11 19:17:28 +0600

    practice sanctum api for student and project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,93 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use  App\Models\Proejct;
 class ProjectController extends Controller
 {
-    //
+    //CREATE PROJECT API    
+    public function createProject(Request $request)
+    {
+        //validation 
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'duration' => 'required'
+        ]);
+
+        //student id
+        $student_id = auth()->user()->id;
+
+        //create data
+        $project = new Proejct();
+        $project->student_id = $student_id;
+        $project->name = $request->name;
+        $project->description = $request->description;
+        $project->duration = $request->duration;
+        $project->save();
+
+        //send response
+        return response()->json([
+            'status' => 1,
+            'message' => 'Project has been created'
+        ],200);
+
+
+        
+    }
+
+    //LIST PROJECT API
+    public function listProject()
+    {
+        $student_id = auth()->user()->id;
+
+        $project = Proejct::where('student_id', $student_id)->get();
+        return response()->json([
+            'status' => 1,
+             'message' => 'Student Projects',
+             'data' => $project
+        ], 200);
+        
+    }
+
+    //SINGLE PROJECT API
+    public function singleProject($id)
+    {
+        $student_id = auth()->user()->id;
+        if(Proejct::where(['id' => $id, 'student_id' =>   $student_id])->exists())
+        {
+            $details = Proejct::where(['id' => $id, 'student_id' =>   $student_id])->get();
+            return response()->json([
+                'status' => 1,
+                'message' => 'Project Details',
+                'data' => $details
+            ],201);
+
+        }else{
+            return response()->json([
+                'status' => 0,
+                'message' => 'Project Not Found'
+            ],404);
+        }
+    }
+
+    //DELETE PROJECT API
+    public function deleteProject($id)
+    {
+        $student_id = auth()->user()->id;
+        if(Proejct::where(['id' => $id, 'student_id' =>   $student_id])->exists())
+        {
+            $project = Proejct::where(['id' => $id, 'student_id' =>   $student_id])->first();
+            $project->delete();
+            return response()->json([
+                'status' => 1,
+                'message' => 'Project Deleted Successfully',
+            ],201);
+
+        }else{
+            return response()->json([
+                'status' => 0,
+                'message' => 'Project Not Found'
+            ],404);
+        } 
+    }
 }
--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -4,8 +4,97 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Student;
+use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-    //
+    //REGISTER API
+    public function register(Request $request)
+    {
+        //validation
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:students',
+            'password' => 'required|confirmed',
+        ]);
+
+        //create data
+        $student = new Student();
+        $student->name = $request->name;
+        $student->email = $request->email;
+        $student->password = Hash::make($request->password);
+        $student->phone_no = isset($request->phone_no) ? $request->phone_no:"";
+        $student->save();
+
+        //send response
+        return response()->json([
+            'status' => 200,
+            'message' => 'student register successfully'
+        ]);
+    }
+
+    //LOGIN API
+    public function login(Request $request)
+    {
+        //validation
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //check student
+        $student = Student::where('email', $request->email)->first();
+
+        if(isset($student->id))
+        {
+            if(Hash::check($request->password, $student->password))
+            {
+                //create token
+                $token = $student->createToken("auth_token")->plainTextToken;
+
+                //send response
+                return response()->json([
+                    'status' => 1,
+                    'message' => "Student Logged in Successfully",
+                    'access_token' => $token 
+                ], 200);
+
+
+            }else{
+                return response()->json([
+                    'status' => 0,
+                    'message' => "Password didn't match"
+                ], 404);
+            }
+
+        }else{
+            return response()->json([
+                'status' => 0,
+                'message' => 'student  Not Found'
+            ], 404);
+        }
+        
+    }
+
+    //PROFILE API
+    public function profile()
+    {
+        return response()->json([
+            'status' => 1,
+            'message' => 'Student Profile Information',
+            'data' => auth()->user()
+        ],200);
+        
+    }
+
+    //LOGOUT API
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => 1,
+            'message' => 'Student Logged Out Successfully',
+        ],200);
+    }
 }
